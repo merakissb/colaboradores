e7e118a31355fa4e675de9cb852dcbc0be9da463
merakissb <mbustosquiroz@gmail.com>
2023-04-28 13:40:27 -0400

    feat: add SearchBar component

--- src/components/partners/PartnerContainer.jsx
+++ src/components/partners/PartnerContainer.jsx
@@ -9,6 +9,7 @@ import Dictionary from '../utils/dictionary/es';
 
 const PartnerContainer = () => {
   const [partners, setPartners] = useState(partnersData);
+  const [searchTerm, setSearchTerm] = useState('');
 
   const handleAddPartner = (newPartner) => {
     setPartners([...partners, newPartner]);
@@ -19,12 +20,26 @@ const PartnerContainer = () => {
     addPartnerService(partner)
   };
 
+  const handleSearch = (searchValue) => {
+    setSearchTerm(searchValue);
+  };
+
+
+  const filteredPartners = partners.filter((partner) => {
+    const partnerName = partner.name.toLowerCase();
+    const partnerEmail = partner.email.toLowerCase();
+    const search = searchTerm.toLowerCase();
+
+    return partnerName.includes(search) || partnerEmail.includes(search);
+  });
+
+
   return (
     <Container fluid>
-      <SearchBar />
+      <SearchBar onSearch={handleSearch} />
         <h3 className='mb-4'>{Dictionary.container.title}</h3>
         <PartnerForm onSubmit={handleSubmit} />
-        <PartnerList partners={partners} />
+        <PartnerList partners={filteredPartners} />
     </Container>
   );
 }
--- src/components/search-bar/SearchBar.jsx
+++ src/components/search-bar/SearchBar.jsx
@@ -1,38 +1,26 @@
-import React, { useState } from "react";
-import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import React from "react";
+import { Col, Container, Form, Row } from "react-bootstrap";
 import Dictionary from '../utils/dictionary/es';
 
-const SearchBar = () => {
-  const [inputValue, setInputValue] = useState('');
+const SearchBar = ({ onSearch })  => {
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-  };
-
-  const handleChange = ({ target: { value } }) => {
-    setInputValue(value);
+  const handleChange = (event) => {
+    const searchValue = event.target.value;
+    onSearch(searchValue);
   };
 
   return (
     <Container className="mt-5">
       <Row className="justify-content-center pb-3">
         <Col sm={6}>
-          <Form className="d-flex" onSubmit={handleSubmit}>
+          <Form className="d-flex">
             <Form.Control
               type="search"
               placeholder={Dictionary.searchBar.placeholder}
               className="me-2 rounded-pill"
               aria-label="Search"
-              value={inputValue}
               onChange={handleChange}
             />
-            <Button
-              className="rounded-pill"
-              variant="primary"
-              disabled={!inputValue.trim()}
-            >
-              {Dictionary.buttons.search}
-            </Button>
           </Form>
         </Col>
       </Row>
